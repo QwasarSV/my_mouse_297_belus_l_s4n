9a335c2b71a2d7b5af47f06b54b44a8291edbe3d
LBelus <lorris.belus@icloud.com>
2023-03-21 02:49:33 +0000

    modified to accomodate queue and function pointers

--- include/my_stack.h
+++ include/my_stack.h
@@ -3,10 +3,21 @@
 
 #include <main_header.h>
 
-extern char* stack[];
-extern int top;
 
-int     push(char* token);
-char*   pop();
+#define _SIZE_STACK_ 250
+
+#ifndef __MY_STACK__
+#define __MY_STACK__
+struct stack
+{
+    char* data[_SIZE_STACK_];
+    int top;
+};
+typedef struct stack stack_t;
+#endif
+
+int     push(stack_t stack, char* token);
+char*   pop(stack_t stack);
+bool    is_empty(stack_t stack);
 
 #endif
--- src/my_stack/stack_fn.c
+++ src/my_stack/stack_fn.c
@@ -1,37 +1,48 @@
 #include <main_header.h>
 
-char*   stack[MAX_INPUT_TOKENS];
-int     top = -1;
 
-int push(char* token)
+int push(stack_t stack, char* token)
 {
-    if (top > MAX_INPUT_TOKENS) 
+    if (stack.top > _SIZE_STACK_) 
     {
         write(STDERR_FILENO, OVERFLOW, SIZE_OVERF);
         return EXIT_FAILURE;
     }
-    free(stack[top + 1]);
-    stack[++top] = strdup(token);
+    free(stack.data[stack.top + 1]);
+    stack.data[++stack.top] = strdup(token);
     return EXIT_SUCCESS;
 }
 
-char* pop()
+char* pop(stack_t stack)
 {
-    if (top > MAX_INPUT_TOKENS) 
+    if (stack.top > _SIZE_STACK_) 
     {
         write(STDERR_FILENO, UNDERFLOW, SIZE_UNDERFLOW);
         return NULL;
     }
-    return stack[top--];
+    return stack.data[stack.top--];
 }
 
-int print_stack()
+bool is_empty(stack_t stack)
 {
-    printf("printing stack top is %i:", top);
-    int index = 0; 
-    while (index <= top)
+    if (stack.top == -1)
     {
-        printf("|%s|",stack[index]);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+int print_stack(stack_t stack)
+{
+    printf("printing stack top is %i:", stack.top);
+    int index = 0; 
+    while (index <= stack.top)
+    {
+        printf("|%s|",stack.data[index]);
         index += 1;
     }
     printf("\n");
